9cf870242bb394ab8149172292a625a8efcdb036
SparshJain2000 <jainsparsh0801@gmail.com>
2020-11-09 01:05:20 +0530

    basic graphql setup

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
-const bodyParser = require("body-parser");
-const express = require("express");
+const bodyParser = require("body-parser"),
+    express = require("express"),
+    { graphqlHTTP } = require("express-graphql"),
+    { buildSchema } = require("graphql");
 app = express();
 const PORT = 8080;
 app.use(bodyParser.json());
-app.get("/", (req, res) => {
-    res.json({ message: "hello world" });
-});
+//define the structure of graphql
+//query - get
+//mutation - post
+// ! - not nullable
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema: buildSchema(`
+        type RootQuery{
+            events: [String!]!
+        }    
+        type RootMutation{
+            createEvent(name: String): String
+        }
+        schema{
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        //  resolvers here
+        rootValue: {
+            events: () => {
+                return ["🔥", "❤", "🎊"];
+            },
+            createEvent: (args) => {
+                const eventName = args.name;
+                return eventName;
+            },
+        },
+        graphiql: true,
+    }),
+);
+
 app.listen(PORT, (req, res) => {
     console.log(`Listening to ${PORT} 🎊`);
 });
