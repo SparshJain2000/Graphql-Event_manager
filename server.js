868982f6e6601cc7cbbc66dcaa612b166254d789
SparshJain2000 <jainsparsh0801@gmail.com>
2020-11-09 23:30:09 +0530

    setup mongoDB + methods for events collections

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const bodyParser = require("body-parser"),
     express = require("express"),
     { graphqlHTTP } = require("express-graphql"),
-    { buildSchema } = require("graphql");
+    { buildSchema } = require("graphql"),
+    mongoose = require("mongoose"),
+    Event = require("./models/event.model");
+
 app = express();
+require("dotenv").config();
 const PORT = 8080;
 app.use(bodyParser.json());
-const events = [];
 // * define the structure of graphql
 // * query - get
 // * mutation - post
@@ -13,51 +16,78 @@ const events = [];
 app.use(
     "/graphql",
     graphqlHTTP({
-        // ? define schema
+        // todo define schema (types) and resolvers
         schema: buildSchema(`
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: String!
-            date: String!
-        }
-        input EventInput {
-            title: String!
-            description: String!
-            price: Float!
-        }
-        // todo define types
-        type RootQuery{
-            events: [Event!]!
-        }    
-        type RootMutation{
-            createEvent(eventInput : EventInput): Event
-        }
-        schema{
-                query: RootQuery
-                mutation: RootMutation
+            type Event {
+                _id: ID!
+                title: String!
+                description: String!
+                price: String!
+                date: String!
+            }   
+            input EventInput {
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
             }
+            
+            type RootQuery{
+                events: [Event!]!
+            }    
+            type RootMutation{
+                createEvent(eventInput : EventInput): Event
+            }
+            schema{
+                    query: RootQuery
+                    mutation: RootMutation
+                }
         `),
-        // ? resolvers here
+        // * resolvers here
         rootValue: {
             events: () => {
-                return events;
+                return Event.find()
+                    .then((res) => {
+                        return res.map((event) => {
+                            return { ...event._doc };
+                        });
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        throw err;
+                    });
             },
             createEvent: (args) => {
-                const event = {
-                    _id: Math.random().toString(),
+                const event = new Event({
                     ...args.eventInput,
-                    date: new Date().toISOString(),
-                };
-                events.push(event);
-                return event;
+                    date: new Date(args.eventInput.date),
+                });
+                // * return to perform async
+                return event
+                    .save()
+                    .then((res) => {
+                        console.log({ ...res._doc });
+                        return { ...res._doc };
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        throw err;
+                    });
             },
         },
         graphiql: true,
     }),
 );
 
-app.listen(PORT, (req, res) => {
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => {
+        console.log("connected to MONGO");
+    });
+app.listen(PORT, () => {
     console.log(`Listening to ${PORT} 🎊`);
 });
