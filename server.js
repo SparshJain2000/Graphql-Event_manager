cda2ebb8f012659d859a9b041737b0d42eddc3b1
SparshJain2000 <jainsparsh0801@gmail.com>
2020-11-10 01:57:37 +0530

    link user & events (creator & createdevents feild)

--- server.js
+++ server.js
@@ -11,6 +11,35 @@ app = express();
 require("dotenv").config();
 const PORT = 8080;
 app.use(bodyParser.json());
+//helper functions
+const user = (id) => {
+    return User.findById(id)
+        .then((user) => {
+            return {
+                ...user._doc,
+                createdEvents: events.bind(this, user._doc.createdEvents),
+            };
+        })
+        .catch((err) => {
+            console.log(err);
+            throw err;
+        });
+};
+const events = (ids) => {
+    return Event.find({ _id: { $in: ids } })
+        .then((events) => {
+            return events.map((event) => {
+                return {
+                    ...event._doc,
+                    creator: user.bind(this, event.creator),
+                };
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            throw err;
+        });
+};
 // * define the structure of graphql
 // * query - get
 // * mutation - post
@@ -26,11 +55,13 @@ app.use(
                 description: String!
                 price: String!
                 date: String!
+                creator: User!
             }   
             type User {
                 _id:ID!
                 email:String!
                 password:String
+                createdEvents :[Event!]
             }
             input UserInput{
                 email:String!
@@ -61,7 +92,10 @@ app.use(
                 return Event.find()
                     .then((res) => {
                         return res.map((event) => {
-                            return { ...event._doc };
+                            return {
+                                ...event._doc,
+                                creator: user.bind(this, event._doc.creator),
+                            };
                         });
                     })
                     .catch((err) => {
@@ -81,7 +115,10 @@ app.use(
                     .save()
                     .then((res) => {
                         console.log({ ...res._doc });
-                        createdEvent = { ...res._doc };
+                        createdEvent = {
+                            ...res._doc,
+                            creator: user.bind(this, res._doc.creator),
+                        };
                         return User.findById("5fa993f1dd57e62c14324b98");
                     })
                     .then((user) => {
