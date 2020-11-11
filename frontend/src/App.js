dff4dc08b83dcad3f7e7fc11a5ae7d6391528810
SparshJain2000 <jainsparsh0801@gmail.com>
2020-11-12 03:03:01 +0530

    Toggle theme button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,62 @@
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component } from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import Bookings from "./components/bookings.component";
-import Auth from "./components/auth.component";
-import Events from "./components/events.component";
-function App() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Redirect from='/' to='auth' exact />
+import Bookings from "./pages/bookings.page";
+import Auth from "./pages/auth.page";
+import Events from "./pages/events.page";
+import Navbar from "./components/navbar.component";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faSun,
+    faMoon,
+    faEye,
+    faLightbulb,
+} from "@fortawesome/free-solid-svg-icons";
+class App extends Component {
+    componentDidMount() {
+        console.log(document.body.classList);
+        if (localStorage.getItem("theme")) {
+            document.body.classList = localStorage.getItem("theme");
+        } else localStorage.setItem("theme", "light");
+    }
+    changeTheme() {
+        console.log(localStorage.getItem("theme"));
+        if (localStorage.getItem("theme")) {
+            const theme =
+                localStorage.getItem("theme") === "light" ? "dark" : "light";
 
-                <Route path='/auth' component={Auth} />
-                <Route path='/bookings' component={Bookings} />
-                <Route path='/events' component={Events} />
-            </Switch>
-        </BrowserRouter>
-    );
+            document.body.classList = theme;
+            localStorage.setItem("theme", theme);
+        } else localStorage.setItem("theme", "light");
+    }
+    render() {
+        return (
+            <BrowserRouter>
+                <React.Fragment>
+                    <Navbar />
+                    <button
+                        className='btn btn-round btn-theme'
+                        onClick={this.changeTheme}>
+                        <FontAwesomeIcon
+                            icon={
+                                localStorage.getItem("theme") === "light"
+                                    ? faLightbulb
+                                    : faLightbulb
+                            }
+                        />
+                    </button>
+                    <main>
+                        <Switch>
+                            <Redirect from='/' to='auth' exact />
+                            <Route path='/auth' component={Auth} />
+                            <Route path='/bookings' component={Bookings} />
+                            <Route path='/events' component={Events} />
+                        </Switch>
+                    </main>
+                </React.Fragment>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
