def4b32489b6660744bc37646551945b9efaf554
SparshJain2000 <jainsparsh0801@gmail.com>
2020-11-13 01:01:52 +0530

    context for login data

---
+++ frontend/src/context/auth-context.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+export default createContext({
+    token: null,
+    userId: null,
+    login: (token, userId, tokenExpiration) => {},
+    logout: () => {},
+});
