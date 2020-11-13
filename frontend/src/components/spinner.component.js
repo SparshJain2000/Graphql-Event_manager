67bd6d287ec1c362ff60695221be4c2824a46de9
SparshJain2000 <jainsparsh0801@gmail.com>
2020-11-14 01:56:43 +0530

    create spinner component

---
+++ frontend/src/components/spinner.component.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../stylesheets/spinner.css";
+const Spinner = ({ height }) => {
+    return (
+        <div class='loadingio-spinner-ripple-woha8w4fq9d'>
+            <div class='ldio-fz12uwmsvs'>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+export default Spinner;
