01dec6daabbbc10df72c6ccdf184c816e70f183e
SparshJain2000 <jainsparsh0801@gmail.com>
2020-11-17 00:14:24 +0530

    update event and pass updated event to eventslist

--- frontend/src/components/event.component.js
+++ frontend/src/components/event.component.js
@@ -1,15 +1,100 @@
 import Axios from "axios";
 import React, { useState, useContext } from "react";
 import { useHistory } from "react-router";
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import {
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    FormGroup,
+    Input,
+    Label,
+} from "reactstrap";
 import "../stylesheets/event.css";
 import AuthContext from "../context/auth-context";
-
-export default Event = ({ event, user }) => {
+const formatDate = (date) => {
+    const now = new Date(date);
+    now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
+    return now.toISOString().slice(0, -1);
+};
+export default Event = ({ event, user, updateEventsList }) => {
     const context = useContext(AuthContext);
     const history = useHistory();
     const [modal, setModal] = useState(false);
+    const [modalUpdate, setModalUpdate] = useState(false);
+    const [data, setData] = useState(event);
     const toggleModal = () => setModal(!modal);
+    const toggleModalUpdate = () => setModalUpdate(!modalUpdate);
+    const updateEvent = () => {
+        let { creator, ...updatedEvent } = data;
+        updatedEvent.creatorId = creator._id;
+        updatedEvent.price = Number(data.price);
+
+        const valid =
+            data.title.trim().length > 0 &&
+            data.description.trim().length > 0 &&
+            data.price >= 0 &&
+            data.date.trim().length > 0;
+        console.log(context.token);
+        if (valid) {
+            console.log(updatedEvent);
+            let config = {
+                headers: {
+                    Authorization: `Bearer ${context.token}`,
+                },
+            };
+            const query = `
+                mutation UpdateEvent($_id:String!,$title:String!,$description:String!,$date:String!,$price:Float!,$creatorId:String!){
+                updateEvent(eventInput:{
+                    _id:$_id
+                    title:$title
+                    description:$description
+                    price:$price
+                    date:$date
+                    creatorId:$creatorId
+                }){
+                    title
+                    price
+                    _id    
+                    date
+                    description
+                    creator{
+                        _id
+                        email
+                    }
+
+                }
+                }
+            `;
+            // updateEventsList(updatedEvent);
+
+            Axios.post(
+                `${process.env.REACT_APP_API_URL}`,
+                { query, variables: updatedEvent },
+                config,
+            )
+                .then((result) => {
+                    console.log(result);
+                    const data = result?.data?.data;
+                    console.log(data);
+                    if (!data.createEvent && result?.data.errors) {
+                        context.logout();
+                        history.push("/auth/signin");
+                    } else {
+                        updateEventsList(data.updateEvent);
+                    }
+                    toggleModalUpdate();
+                })
+                .catch((err) => console.log(err.response));
+        }
+    };
+    const handleChange = (e) => {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value,
+        });
+    };
     const bookEvent = () => {
         const query = `
             mutation {
@@ -69,7 +154,7 @@ export default Event = ({ event, user }) => {
                 className='col-12 col-sm-3 px-0 my-auto d-flex justify-content-center justify-content-sm-end'
                 style={{ height: "fit-content" }}>
                 {user.userId === event.creator._id ? (
-                    <Button color='secondary' onClick={toggleModal}>
+                    <Button color='secondary' onClick={toggleModalUpdate}>
                         Edit Event
                     </Button>
                 ) : (
@@ -104,6 +189,57 @@ export default Event = ({ event, user }) => {
                         Book Event
                     </Button>
                     <Button color='outline-secondary' onClick={toggleModal}>
+                        Close
+                    </Button>
+                </ModalFooter>
+            </Modal>
+            <Modal isOpen={modalUpdate} toggle={toggleModalUpdate}>
+                <ModalHeader toggle={toggleModalUpdate}>
+                    Update Event
+                </ModalHeader>
+                <ModalBody>
+                    <FormGroup>
+                        <Label>Title</Label>
+                        <Input
+                            onChange={handleChange}
+                            type='text'
+                            defaultValue={event.title}
+                            name='title'></Input>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label>Price</Label>
+                        <Input
+                            onChange={handleChange}
+                            type='number'
+                            defaultValue={event.price}
+                            name='price'></Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Time</Label>
+                        <Input
+                            onChange={handleChange}
+                            type='datetime-local'
+                            defaultValue={formatDate(event.date)}
+                            name='date'></Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Description</Label>
+                        <Input
+                            onChange={handleChange}
+                            type='textarea'
+                            name='description'
+                            defaultValue={event.description}
+                            rows='3'></Input>
+                    </FormGroup>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color='secondary' onClick={updateEvent}>
+                        Update Event
+                    </Button>
+                    <Button
+                        color='outline-secondary'
+                        onClick={toggleModalUpdate}>
                         Close
                     </Button>
                 </ModalFooter>
